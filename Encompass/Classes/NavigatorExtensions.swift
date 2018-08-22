c7e73ec5f3c78d4e937ed7c7040df76460ed38a0
NduatiK <012nkuria@gmail.com>
2018-08-22 11:43:21 +0300

    Moved external navigation responsibilities back into Navigator using extensions

---
+++ Encompass/Classes/NavigatorExtensions.swift
@@ -0,0 +1,42 @@
+//
+//  RouteHandler.swift
+//  Compass
+//
+//  Created by Nduati Kuria on 15/08/2018.
+//
+
+import Foundation
+import Compass
+
+fileprivate var registeredRoutes = [String: Routable]()
+fileprivate var registeredRouters = [String]()
+
+
+extension Navigator {
+    public static func registerRouter<R: RouteConvertible>(_ router: Encompasser<R>) {
+        for path in router.paths {
+            registeredRoutes[path] = router.pathToRoutableMap[path]
+        }
+        registeredRouters.append(String(describing: router))
+        Navigator.routes.append(contentsOf: router.paths)
+    }
+
+    public static func navigate(to location: Location, from currentController: CurrentController) {
+        if let router = registeredRoutes[location.path] {
+            try! router.navigate(to: location, from: currentController)
+            return
+        }
+    }
+
+    public static func navigate(registeredUrn: String) -> Bool {
+        for routerName in registeredRouters {
+            let urn = routerName + registeredUrn
+            if registeredRoutes[urn] != nil {
+                var location = Location(path: urn, payload: nil)
+                Navigator.handle?(location)
+                return true
+            }
+        }
+        return false
+    }
+}
